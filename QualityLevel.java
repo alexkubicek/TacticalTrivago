e375962d97c327ca73bfae2dcfd42d21327a0dd1
alexkubicek <127884841+alexkubicek@users.noreply.github.com>
2023-03-19 14:25:50 -0500

    Update QualityLevel.java
    add getRate() implementation
    
    Signed-off-by: alexkubicek <127884841+alexkubicek@users.noreply.github.com>

--- QualityLevel.java
+++ QualityLevel.java
@@ -1,11 +1,19 @@
 public enum QualityLevel {
-  Double maxDailyRate;
-  EXECUTIVE{
-  },
-  BUSINESS{
-  },
-  COMFORT{
-  },
-  ECONOMY{
-  };
+  EXECUTIVE,
+	BUSINESS,
+	COMFORT,
+	ECONOMY;
+
+	Double getRate() {
+		if(EXECUTIVE != null) {
+			return 10.0; //change when decided
+		} else if(BUSINESS != null) {
+			return 7.0;
+		} else if(COMFORT != null) {
+			return 5.0;
+		}else if(ECONOMY != null) {
+			return 3.0;
+		}
+		return null;
+	}
 }
