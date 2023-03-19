fac0e8a21b28fed6d0ac42e1064c4af07f1eb765
alexkubicek <127884841+alexkubicek@users.noreply.github.com>
2023-03-19 07:26:11 -0500

    Add associations
    Signed-off-by: alexkubicek <127884841+alexkubicek@users.noreply.github.com>

--- Guest.java
+++ Guest.java
@@ -1,3 +1,8 @@
 public class Guest extends Person {
   Boolean isCorporate;
+  
+  //associations
+  List<Reservation> upcomingReservations;
+  List<Address> addresses; //index 0 being default to use
+  List<CreditCard> paymentMethods; //index 0 being default to use
 }
