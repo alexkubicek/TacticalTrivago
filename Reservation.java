e97db7027080b3ae3443eaa986389186a7502800
alexkubicek <127884841+alexkubicek@users.noreply.github.com>
2023-03-19 07:53:50 -0500

    Add operation contracts
    Signed-off-by: alexkubicek <127884841+alexkubicek@users.noreply.github.com>

--- Reservation.java
+++ Reservation.java
@@ -2,9 +2,22 @@ public class Reservation {
   private Date startDate;
   private Date endDate;
   private Integer nights; //derived from start and end dates
-  private Double rate;
+  private Double rate; //derived from individual room rates
   
   //associations
   private Guest guest;
   private List<Room> rooms;
+  
+  //operation contracts
+  void modifyTime(Date start, Date end) {}
+  void modifyRoom(List<Room> newRooms) {}
+  void checkIn() {
+    //change status of associated rooms
+  }
+  void checkOut() {
+    //change status of associated rooms
+  }
+  Double calculateTotal(){
+    //return rate * nights
+  }
 }
