d8fb7d2a991ff6c77e46158446d7530ff11e2aa3
Earthground <tristanbrown377@gmail.com>
2023-03-20 18:05:06 -0500

    CreditCard and Payment Updated

--- CreditCard.java
+++ CreditCard.java
@@ -1,9 +1,40 @@
-import java.util.Date;
-
 public class CreditCard {
-  Integer cardNum, cvv;
-  Date expiration;
-  
-  //associations
-  User p;
+    Integer cardNum, cvv;
+    Date expiration;
+
+    //associations
+    Person p;
+
+    public CreditCard(){}
+    public CreditCard(Date date, Integer cardID, Integer threeFancyDigits){
+        this.cardNum = cardID;
+        this.expiration = date;
+        this.cvv = threeFancyDigits;
+    }
+
+    public static void printDate(Date c){
+        SimpleDateFormat f = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        System.out.println(f.format(c.getTime()));
+    }
+    public void displayInfo(){
+        System.out.print("Card Number: " + this.cardNum + "\ncvv: "
+                + this.cvv + "Expiration Date: ");
+        printDate(this.expiration);
+    }
+
+    public void setCardNum(Integer cardNum) {
+        this.cardNum = cardNum;
+    }
+
+    public void setCvv(Integer cvv) {
+        this.cvv = cvv;
+    }
+
+    public void setExpiration(Date expiration) {
+        this.expiration = expiration;
+    }
+
+    public void setP(Person p) {
+        this.p = p;
+    }
 }
