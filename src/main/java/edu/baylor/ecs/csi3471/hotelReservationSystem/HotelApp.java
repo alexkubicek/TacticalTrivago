9ac2de3a75195f6bfa428ade0fb8d5d3c46c77fa
Thomas Cho <thomas_cho1@baylor.edu>
2023-03-30 23:50:20 -0500

    User button added in app (Same thing exists in ReservationUI)

--- src/main/java/edu/baylor/ecs/csi3471/hotelReservationSystem/HotelApp.java
+++ src/main/java/edu/baylor/ecs/csi3471/hotelReservationSystem/HotelApp.java
@@ -1,6 +1,8 @@
 package edu.baylor.ecs.csi3471.hotelReservationSystem;
 
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
 import javax.swing.*;
 import java.awt.GridLayout;
@@ -34,8 +36,38 @@ public class HotelApp extends javax.swing.JFrame{
         userFormPanel.add(new JLabel("Password: "));
         userFormPanel.add(new JTextField(20));
         userFormPanel.add(new JLabel("Type: "));
-        JComboBox<String> genderComboBox = new JComboBox<>(new String[]{"Admin", "Clerk", "Other"});
-        userFormPanel.add(genderComboBox);
+        JComboBox<String> ComboBox = new JComboBox<>(new String[]{"Admin", "Clerk", "Other"});
+        userFormPanel.add(ComboBox);
+
+        JPanel buttonPane = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        JButton submitButton = new JButton("Submit");
+        buttonPane.add(submitButton);
+        userFormPanel.add(buttonPane);
+
+        submitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String firstName = ((JTextField) userFormPanel.getComponent(1)).getText();
+                String lastName = ((JTextField) userFormPanel.getComponent(3)).getText();
+                String id = ((JTextField) userFormPanel.getComponent(5)).getText();
+                String password = ((JTextField) userFormPanel.getComponent(7)).getText();
+                String type = (String) ComboBox.getSelectedItem();
+
+                if (type.equals("Admin")) {
+                    /*
+                      We can implement adding feature in here
+                     */
+                    System.out.println("Adding new admin: " + firstName + " " + lastName);
+                } else if (type.equals("Clerk")) {
+                    /*
+                      We can implement adding feature in here later
+                     */
+                    System.out.println("Adding new clerk: " + firstName + " " + lastName);
+                }
+
+            }
+        });
+
 
         pan3.add(userFormPanel);
 
