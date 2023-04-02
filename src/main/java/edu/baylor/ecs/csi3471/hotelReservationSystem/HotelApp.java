3a759343ea80c058f5b26a95d22d9ec8a9f0d696
Thomas Cho <thomas_cho1@baylor.edu>
2023-04-01 22:24:51 -0500

    Test Push

--- src/main/java/edu/baylor/ecs/csi3471/hotelReservationSystem/HotelApp.java
+++ src/main/java/edu/baylor/ecs/csi3471/hotelReservationSystem/HotelApp.java
@@ -60,55 +60,55 @@ public class HotelApp extends javax.swing.JFrame{
 
 //        Implemented in AddUserUI
 
-//        //customTableModel.display();
-//        JPanel pan3 = new JPanel();
-//        JPanel userFormPanel = new JPanel(new GridLayout(0, 2)); // 2 columns
-//        userFormPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10)); // Add padding
-//
-//
-//        userFormPanel.add(new JLabel("First Name: "));
-//        userFormPanel.add(new JTextField(20));
-//        userFormPanel.add(new JLabel("Last Name: "));
-//        userFormPanel.add(new JTextField(20));
-//        userFormPanel.add(new JLabel("ID: "));
-//        userFormPanel.add(new JTextField(20));
-//        userFormPanel.add(new JLabel("Password: "));
-//        userFormPanel.add(new JTextField(20));
-//        userFormPanel.add(new JLabel("Type: "));
-//        JComboBox<String> ComboBox = new JComboBox<>(new String[]{"Admin", "Clerk", "Other"});
-//        userFormPanel.add(ComboBox);
-//
-//        JPanel buttonPane = new JPanel(new FlowLayout(FlowLayout.CENTER));
-//        JButton submitButton = new JButton("Submit");
-//        buttonPane.add(submitButton);
-//        userFormPanel.add(buttonPane);
-//
-//        submitButton.addActionListener(new ActionListener() {
-//            @Override
-//            public void actionPerformed(ActionEvent e) {
-//                String firstName = ((JTextField) userFormPanel.getComponent(1)).getText();
-//                String lastName = ((JTextField) userFormPanel.getComponent(3)).getText();
-//                String id = ((JTextField) userFormPanel.getComponent(5)).getText();
-//                String password = ((JTextField) userFormPanel.getComponent(7)).getText();
-//                String type = (String) ComboBox.getSelectedItem();
-//
-//                if (type.equals("Admin")) {
-//                    /*
-//                      We can implement adding feature in here
-//                     */
-//                    System.out.println("Adding new admin: " + firstName + " " + lastName);
-//                } else if (type.equals("Clerk")) {
-//                    /*
-//                      We can implement adding feature in here later
-//                     */
-//                    System.out.println("Adding new clerk: " + firstName + " " + lastName);
-//                }
-//
-//            }
-//        });
+        //customTableModel.display();
+        JPanel pan3 = new JPanel();
+        JPanel userFormPanel = new JPanel(new GridLayout(0, 2)); // 2 columns
+        userFormPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10)); // Add padding
 
-        AddUserUI UserUI = new AddUserUI();
-        JPanel pan3 = UserUI.AddUser();
+
+        userFormPanel.add(new JLabel("First Name: "));
+        userFormPanel.add(new JTextField(20));
+        userFormPanel.add(new JLabel("Last Name: "));
+        userFormPanel.add(new JTextField(20));
+        userFormPanel.add(new JLabel("ID: "));
+        userFormPanel.add(new JTextField(20));
+        userFormPanel.add(new JLabel("Password: "));
+        userFormPanel.add(new JTextField(20));
+        userFormPanel.add(new JLabel("Type: "));
+        JComboBox<String> ComboBox = new JComboBox<>(new String[]{"Admin", "Clerk", "Other"});
+        userFormPanel.add(ComboBox);
+
+        JPanel buttonPane = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        JButton submitButton = new JButton("Submit");
+        buttonPane.add(submitButton);
+        userFormPanel.add(buttonPane);
+
+        submitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String firstName = ((JTextField) userFormPanel.getComponent(1)).getText();
+                String lastName = ((JTextField) userFormPanel.getComponent(3)).getText();
+                String id = ((JTextField) userFormPanel.getComponent(5)).getText();
+                String password = ((JTextField) userFormPanel.getComponent(7)).getText();
+                String type = (String) ComboBox.getSelectedItem();
+
+                if (type.equals("Admin")) {
+                    /*
+                      We can implement adding feature in here
+                     */
+                    System.out.println("Adding new admin: " + firstName + " " + lastName);
+                } else if (type.equals("Clerk")) {
+                    /*
+                      We can implement adding feature in here later
+                     */
+                    System.out.println("Adding new clerk: " + firstName + " " + lastName);
+                }
+
+            }
+        });
+
+        //AddUserUI UserUI = new AddUserUI();
+        //JPanel pan3 = UserUI.AddUser();
         tabs.addTab("Add rooms", null, pan1);
         tabs.addTab("Reserve rooms", null, pan2);
         tabs.addTab("Add User", null, pan3);
