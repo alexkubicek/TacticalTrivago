92a92ee3f453554b0f2c0de213973614464c4999
kaylynnbeard <kaylynn_beard1@baylor.edu>
2023-04-21 19:20:35 -0500

    MakeReservation use case

--- src/main/java/edu/baylor/ecs/csi3471/hotelReservationSystem/GUI/RoomTableModel.java
+++ src/main/java/edu/baylor/ecs/csi3471/hotelReservationSystem/GUI/RoomTableModel.java
@@ -1,66 +1,75 @@
+/**
+ * file: RoomTableModel
+ * author: KayLynn Beard
+ *
+ * Filterable, scrollable, selectable table object that
+ * displays all (available?) rooms in the hotel
+ */
+
+// TODO: make it pretty (change dimensions, font, font size, alignment, $ for rates, etc...)
+// TODO: update table function with chosen dates?
+
 package edu.baylor.ecs.csi3471.hotelReservationSystem.GUI;
 
-import java.util.Date;
-import java.util.Objects;
+import edu.baylor.ecs.csi3471.hotelReservationSystem.backend.*;
 
+import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.util.Date;
+import java.util.List;
+import net.coderazzi.filters.gui.*;
 
-import edu.baylor.ecs.csi3471.hotelReservationSystem.backend.BedType;
-import edu.baylor.ecs.csi3471.hotelReservationSystem.backend.Hotel;
-import edu.baylor.ecs.csi3471.hotelReservationSystem.backend.QualityLevel;
-import edu.baylor.ecs.csi3471.hotelReservationSystem.backend.Room;
+public class RoomTableModel extends JPanel {
+    protected Hotel hotel;
+    protected JTable table;
+    protected Date startDate, endDate;
 
-public class RoomTableModel extends DefaultTableModel {
-    public static final Class<?>[] columnClass = new Class[] {Integer.class, QualityLevel.class, Integer.class, BedType.class, boolean.class, Double.class};
-    public static final String[] columnNames = {"Room Number", "Quality Level", "Bed Count", "Bed Size", "Smoking", "Rate"};
-    @Override
-    public int getColumnCount() {
-        return columnNames.length;
-    }
+    private static final int MAX_ROOMS = 40;
+    private static final int NUM_COLUMNS = 6;
+    final Class<?>[] columnClass = new Class[] {
+            String.class, String.class, BedType.class, QualityLevel.class,
+            Boolean.class, Double.class};
+    private static final String[] columnNames = {
+            "Room Number", "Bed Count", "Bed Size", "Quality Level",
+            "Smoking", "Room Rate" };
+    private static Object[][] rooms = new Object[MAX_ROOMS][NUM_COLUMNS];
 
-    @Override
-    public Class<?> getColumnClass(int columnIndex) {
-        return columnClass[columnIndex];
-    }
-
-    @Override
-    public String getColumnName(int col) {
-        return columnNames[col];
-    }
-    @Override
-    public boolean isCellEditable(int row, int column) {return false;}
-
-    public void populate() {
-        Hotel.rooms.forEach(r->{
-            Object[] curRoom = {r.getRoomNumber(), r.getQuality(), r.getBedCount(), r.getBedSize(), r.getSmoking(), r.getQuality().getRate()};
-            this.addRow(curRoom);
-        });
-    }
-
-    public RoomTableModel() {
+    public RoomTableModel(Hotel h, Guest g){
         super();
-        this.populate();
+        this.hotel = h;
+        // get all rooms from hotel
+        loadRoomsIntoTable(hotel.getRooms());
+        // create table of rooms
+        DefaultTableModel model = new DefaultTableModel(rooms, columnNames) {
+            @Override
+            public boolean isCellEditable(int row, int column) {return false;}
+            @Override
+            public Class<?> getColumnClass(int columnIndex) {return columnClass[columnIndex];}
+        };
+        table = new JTable(model);
+        // set dimensions of table
+        table.setPreferredScrollableViewportSize(new Dimension(500, 300));
+        table.setFillsViewportHeight(true);
+        // only reserve one room at a time
+        table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        // make it scrollable
+        JScrollPane scrollPane = new JScrollPane(table);
+        add(scrollPane);
+        // add filters for each column
+        TableFilterHeader filterHeader = new TableFilterHeader(table, AutoChoices.ENABLED);
     }
-
-    public void filter(QualityLevel ql, Integer bedCount, BedType bedSize, Boolean s, Date start, Date end) {
-        this.populate();
-        for(int i = 0; i < this.getRowCount(); i++) {
-            Room curRoom = Hotel.getRoom((int)(this.getValueAt(i, 0)));
-            if(ql != QualityLevel.ALL && curRoom.getQuality() != ql) {
-                this.removeRow(i--);
-            } else if(bedCount != null && (!Objects.equals(curRoom.getBedCount(), bedCount))) {
-                this.removeRow(i--);
-            } else if(bedSize != BedType.ALL && this.getValueAt(i, 3) != bedSize) {
-                this.removeRow(i--);
-            } else if(s != null && s != this.getValueAt(i, 4)) {
-                this.removeRow(i--);
-            } else if(start != null){
-                if(!curRoom.isAvailable(start, end)) {
-                    this.removeRow(i--);
-                }
-            }
+    public void loadRoomsIntoTable(List<Room> roomList){
+        int i = 0;
+        for (Room r : roomList){
+            rooms[i] = new Object[NUM_COLUMNS];
+            rooms[i][0] = r.getRoomNumber();
+            rooms[i][1] = r.getBedCount();
+            rooms[i][2] = r.getBedSize();
+            rooms[i][3] = r.getQuality();
+            rooms[i][4] = r.getSmoking();
+            rooms[i][5] = r.getQuality().getRate();
+            i++;
         }
-
     }
-
 }
