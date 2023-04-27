5eb9e74d14397bc8b4d8140333996c93c2823e4c
kaylynnbeard <kaylynn_beard1@baylor.edu>
2023-04-27 12:48:29 -0500

    Load credit cards

--- src/main/java/edu/baylor/ecs/csi3471/hotelReservationSystem/backend/Guest.java
+++ src/main/java/edu/baylor/ecs/csi3471/hotelReservationSystem/backend/Guest.java
@@ -23,7 +23,7 @@ public class Guest extends User {
         super(line[0], line[1], new AccountInformation(line[2], line[3]));
         isCorporate = Boolean.parseBoolean(line[4]);
         upcomingReservations = new ArrayList<>();
-        this.paymentMethods = new ArrayList<>();
+        paymentMethod = new CreditCard(line);
     }
     public Guest(){
         super();
